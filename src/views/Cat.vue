ac59f62607e369ef2e9ff065e184d8da56bda411
lujunhui <385935631@qq.com>
2020-01-29 21:38:36 +0800

    categroy完成

--- src/views/Cat.vue
+++ src/views/Cat.vue
@@ -12,35 +12,30 @@
         </el-col>
       </el-row>
 
-     <!-- 表格 -->
-       <el-table
-    :data="tableData"
-    border
-    style="width: 100%"
-    row-key="cat_id"
-    :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-    >
-    <el-table-column
-      prop="cat_name"
-      label="分类名称"
-      min-width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="等级">
-       <template>
-          <el-tag size="medium">一级</el-tag>
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="操作">
-      <template>
-          <el-button type="primary" icon="el-icon-edit" size="mini">修改</el-button>
-          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+      <!-- 表格 -->
+      <el-table
+        :data="tableData"
+        border
+        style="width: 100%"
+        row-key="cat_id"
+        :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+        @expand-change="toggleRowExpansion"
+      >
+        <el-table-column prop="cat_name" label="分类名称" min-width="180"></el-table-column>
+        <el-table-column prop="name" label="等级">
+          <template slot-scope="scope">
+            <el-tag size="medium" v-if="scope.row.cat_level===0">一级</el-tag>
+            <el-tag size="medium" type="success" v-if="scope.row.cat_level===1">二级</el-tag>
+            <el-tag size="medium" type="danger" v-if="scope.row.cat_level===2">三级</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="address" label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini">修改</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
@@ -57,6 +52,10 @@ export default {
     this.getTableData()
   },
   methods: {
+    toggleRowExpansion (row, expanded) {
+      console.log(row)
+      console.log(expanded)
+    },
     // 拿到表格数据
     async getTableData () {
       let { data } = await this.$http.get('categories', {
