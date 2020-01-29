62625d1ea8b88ca554bbfb635f214cdc8374d6c6
lujunhui <385935631@qq.com>
2020-01-29 15:09:26 +0800

    表格搭建完成+级联数据添加

--- src/views/GoodsAdd.vue
+++ src/views/GoodsAdd.vue
@@ -27,7 +27,7 @@
         class="tabs"
         :tab-position="tabPosition"
         v-model="activeName"
-        @tab-click="handleClick"
+        :before-leave="leaveTab"
       >
         <el-tab-pane label="基本信息" name="first">
           <!-- 选项卡第一页表单 -->
@@ -50,8 +50,18 @@
             <el-form-item label="数量" prop="num">
               <el-input v-model="ruleForm.num"></el-input>
             </el-form-item>
-            <el-form-item label="商品类型" prop="type">
-              <el-cascader v-model="value" :options="options" @change="handleChange"></el-cascader>
+            <el-form-item label="商品类型" prop="selectThreeCat">
+              <el-cascader
+                v-model="ruleForm.selectThreeCat"
+                :options="CascaderList"
+                @change="handleChange"
+                :props="{
+                    expandTrigger: 'hover',
+                    value:'cat_id',
+                    label:'cat_name'
+                }"
+                clearable
+              ></el-cascader>
             </el-form-item>
           </el-form>
         </el-tab-pane>
@@ -76,300 +86,77 @@ export default {
       // 选项卡第一项表单
       ruleForm: {
         name: '',
-        price: ''
+        price: '',
+        weight: '',
+        num: '',
+        selectThreeCat: ''
+      },
+      // 表单校验规则
+      rules: {
+        name: [{ required: true, message: '请输入名称', trigger: 'blur' }],
+        price: [{ required: true, message: '请输入价格', trigger: 'blur' }],
+        weight: [{ required: true, message: '请输入重量', trigger: 'blur' }],
+        num: [{ required: true, message: '请输入数量', trigger: 'blur' }],
+        selectThreeCat: [
+          { required: true, message: '请选择类型', trigger: 'change' }
+        ]
       },
       value: [],
-      options: [
-        {
-          value: 'zhinan',
-          label: '指南',
-          children: [
-            {
-              value: 'shejiyuanze',
-              label: '设计原则',
-              children: [
-                {
-                  value: 'yizhi',
-                  label: '一致'
-                },
-                {
-                  value: 'fankui',
-                  label: '反馈'
-                },
-                {
-                  value: 'xiaolv',
-                  label: '效率'
-                },
-                {
-                  value: 'kekong',
-                  label: '可控'
-                }
-              ]
-            },
-            {
-              value: 'daohang',
-              label: '导航',
-              children: [
-                {
-                  value: 'cexiangdaohang',
-                  label: '侧向导航'
-                },
-                {
-                  value: 'dingbudaohang',
-                  label: '顶部导航'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          value: 'zujian',
-          label: '组件',
-          children: [
-            {
-              value: 'basic',
-              label: 'Basic',
-              children: [
-                {
-                  value: 'layout',
-                  label: 'Layout 布局'
-                },
-                {
-                  value: 'color',
-                  label: 'Color 色彩'
-                },
-                {
-                  value: 'typography',
-                  label: 'Typography 字体'
-                },
-                {
-                  value: 'icon',
-                  label: 'Icon 图标'
-                },
-                {
-                  value: 'button',
-                  label: 'Button 按钮'
-                }
-              ]
-            },
-            {
-              value: 'form',
-              label: 'Form',
-              children: [
-                {
-                  value: 'radio',
-                  label: 'Radio 单选框'
-                },
-                {
-                  value: 'checkbox',
-                  label: 'Checkbox 多选框'
-                },
-                {
-                  value: 'input',
-                  label: 'Input 输入框'
-                },
-                {
-                  value: 'input-number',
-                  label: 'InputNumber 计数器'
-                },
-                {
-                  value: 'select',
-                  label: 'Select 选择器'
-                },
-                {
-                  value: 'cascader',
-                  label: 'Cascader 级联选择器'
-                },
-                {
-                  value: 'switch',
-                  label: 'Switch 开关'
-                },
-                {
-                  value: 'slider',
-                  label: 'Slider 滑块'
-                },
-                {
-                  value: 'time-picker',
-                  label: 'TimePicker 时间选择器'
-                },
-                {
-                  value: 'date-picker',
-                  label: 'DatePicker 日期选择器'
-                },
-                {
-                  value: 'datetime-picker',
-                  label: 'DateTimePicker 日期时间选择器'
-                },
-                {
-                  value: 'upload',
-                  label: 'Upload 上传'
-                },
-                {
-                  value: 'rate',
-                  label: 'Rate 评分'
-                },
-                {
-                  value: 'form',
-                  label: 'Form 表单'
-                }
-              ]
-            },
-            {
-              value: 'data',
-              label: 'Data',
-              children: [
-                {
-                  value: 'table',
-                  label: 'Table 表格'
-                },
-                {
-                  value: 'tag',
-                  label: 'Tag 标签'
-                },
-                {
-                  value: 'progress',
-                  label: 'Progress 进度条'
-                },
-                {
-                  value: 'tree',
-                  label: 'Tree 树形控件'
-                },
-                {
-                  value: 'pagination',
-                  label: 'Pagination 分页'
-                },
-                {
-                  value: 'badge',
-                  label: 'Badge 标记'
-                }
-              ]
-            },
-            {
-              value: 'notice',
-              label: 'Notice',
-              children: [
-                {
-                  value: 'alert',
-                  label: 'Alert 警告'
-                },
-                {
-                  value: 'loading',
-                  label: 'Loading 加载'
-                },
-                {
-                  value: 'message',
-                  label: 'Message 消息提示'
-                },
-                {
-                  value: 'message-box',
-                  label: 'MessageBox 弹框'
-                },
-                {
-                  value: 'notification',
-                  label: 'Notification 通知'
-                }
-              ]
-            },
-            {
-              value: 'navigation',
-              label: 'Navigation',
-              children: [
-                {
-                  value: 'menu',
-                  label: 'NavMenu 导航菜单'
-                },
-                {
-                  value: 'tabs',
-                  label: 'Tabs 标签页'
-                },
-                {
-                  value: 'breadcrumb',
-                  label: 'Breadcrumb 面包屑'
-                },
-                {
-                  value: 'dropdown',
-                  label: 'Dropdown 下拉菜单'
-                },
-                {
-                  value: 'steps',
-                  label: 'Steps 步骤条'
-                }
-              ]
-            },
-            {
-              value: 'others',
-              label: 'Others',
-              children: [
-                {
-                  value: 'dialog',
-                  label: 'Dialog 对话框'
-                },
-                {
-                  value: 'tooltip',
-                  label: 'Tooltip 文字提示'
-                },
-                {
-                  value: 'popover',
-                  label: 'Popover 弹出框'
-                },
-                {
-                  value: 'card',
-                  label: 'Card 卡片'
-                },
-                {
-                  value: 'carousel',
-                  label: 'Carousel 走马灯'
-                },
-                {
-                  value: 'collapse',
-                  label: 'Collapse 折叠面板'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          value: 'ziyuan',
-          label: '资源',
-          children: [
-            {
-              value: 'axure',
-              label: 'Axure Components'
-            },
-            {
-              value: 'sketch',
-              label: 'Sketch Templates'
-            },
-            {
-              value: 'jiaohu',
-              label: '组件交互文档'
-            }
-          ]
-        }
-      ]
+      CascaderList: []
     }
+  },
+  created () {
+    this.getCascaderList()
   },
   methods: {
     // 选项卡触发逻辑
-    handleClick () {
-      if (this.activeName === 'first') {
-        this.active = 0
+    leaveTab (activeName) {
+      if (activeName !== 'first') {
+        this.validateForm()
+        if (!this.validresult) {
+          return false
+        }
       }
-      if (this.activeName === 'second') {
-        this.active = 1
-      }
-      if (this.activeName === 'third') {
-        this.active = 2
-      }
-      if (this.activeName === 'fourth') {
-        this.active = 3
-      }
-      if (this.activeName === 'fifth') {
-        this.active = 4
-      }
-      if (this.activeName === 'sixth') {
-        this.active = 5
-      }
+    },
+    // 第一项表单校验
+    validateForm (activeName) {
+      this.$refs['ruleForm'].validate(valid => {
+        this.validresult = valid
+        if (valid) {
+          alert('submit!')
+        } else {
+          this.$message.error('请先填写基本信息')
+          return valid
+        }
+      })
+      return this.validresult
+    },
+    // 拿到级联数据
+    async getCascaderList () {
+      var { data: dt } = await this.$http.get('categories', {
+        params: { type: 3 }
+      })
+      //   console.log(dt)
+      // 把获得到的分类列表数据赋予给catList成员
+      this.CascaderList = dt.data
+
+      this.CascaderList.forEach(item => {
+        item.value = item.cat_pid
+        item.label = item.cat_name
+        if (item.children) {
+          item.children.forEach(item1 => {
+            item1.value = item1.cat_pid
+            item1.label = item1.cat_name
+            if (item1.children) {
+              item1.children.forEach(item2 => {
+                item2.value = item2.cat_pid
+                item2.label = item2.cat_name
+              })
+            }
+          })
+        }
+      })
+      console.log(this.CascaderList)
     }
   }
 }
